0fee0b7d1c66817557f8dca5e847f23d4244d589
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2012-07-21 13:45:15 -0700

    rails

---
+++ browserbump/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Browserbump::Application.config.secret_token = 'fd928e3cd1294d2bb89d4a89fe0dcb4d16968b73092ba9b6e8dbd6a819d4261100b29a25b8a8274819734901eaf9770090a599a5abd1ca3c2c97fd5466710181'
