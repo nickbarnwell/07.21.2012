c7e2975ec0752d4e19fad75f265cd4632731741b
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2012-07-22 02:30:41 -0700

    testing rotues

--- app/controllers/bump_controller.rb
+++ app/controllers/bump_controller.rb
@@ -1,4 +1,6 @@
 class BumpController < ApplicationController
+  skip_before_filter :login_required
+
   def index
   end
 
@@ -8,10 +10,21 @@ class BumpController < ApplicationController
       lat: 37.7763675 + georand,
       lon: -122.3926582 + georand,
       acc: intrange(0,100),
-      uid: intrange(0,12313)
-    }
-    render :json => dict
+      uid: intrange(0,12313).to_s
+    }.to_json
+    puts dict
+    puts %x[python mongo/save_event.py '#{dict}']
+    #render :json => dict
+    render :status => 200, :text=>nil
   end
+
+  def fake_get
+    uid = params[:uid]
+    puts %x[python mongo/get_group.py '#{uid}']
+    render :status => 200, :text=>nil
+  end
+
+
 
   def range (min, max)
     (rand * (max-min) + min)
@@ -22,7 +35,7 @@ class BumpController < ApplicationController
   end
 
   def georand()
-    ((range(-1,1) * rand/10))
+    ((range(-1,1) * rand/1000))
   end
 
 end
