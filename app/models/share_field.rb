bf8b22a57218124f6198c9657b4e25b70f00da1f
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2012-07-21 20:49:49 -0700

    Whatahack

--- app/models/share_field.rb
+++ app/models/share_field.rb
@@ -12,4 +12,20 @@ class ShareField < ActiveRecord::Base
     share = true
     save!
   end
+
+  def link_value
+    if field_type =~ /mail-*/
+      return  "<a href='mailto: #{value}'>#{value}</a>"
+    elsif field_type =~ /tel-*/
+      return "<a href='tel:#{value}'>#{value}</a>"
+    end
+
+    case field_type
+    when 'twitter'
+      return "<a href='http://twitter.com/#{value}'>#{value}</a>"
+    when 'facebook'
+      return "<a href='#{user.raw['link']}'>#{user.name}</a>"
+    end
+
+  end
 end
