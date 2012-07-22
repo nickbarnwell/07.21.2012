6e7359b123a3a7a9f4d9eb647a0981bb4066d99c
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2012-07-21 20:20:58 -0700

    Presser logged in fix

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,6 @@
 class LandingController < ApplicationController
   before_filter :logged_in_redirect
+  skip_before_filter :logged_in_redirect, :only => [:test]
   skip_before_filter :login_required
 
   def index
