3d3ce638017778156795e180714fdbb4d518b1ca
Dan Girshovich <dan@crittercism.com>
2012-07-22 02:00:19 -0700

    now autoconnects to mongo from save and get group

--- mongo/get_group.py
+++ mongo/get_group.py
@@ -9,6 +9,7 @@ def main(uid):
 
 if __name__ == '__main__':
 	if len(sys.argv) == 2:
+		c.connect_db()
 		main(sys.argv[1])
 	else:
 		print 'Usage: python get_group.py <uid>'
--- mongo/save_event.py
+++ mongo/save_event.py
@@ -3,7 +3,6 @@ import common as c
 import json
 from Event import Event
 
-
 def main(json_event):
 	d = json.loads(json_event)
 	uid = d['uid']
@@ -14,6 +13,7 @@ def main(json_event):
 
 if __name__ == '__main__':
 	if len(sys.argv) == 2:
+		c.connect_db()
 		main(sys.argv[1])
 	else:
 		print 'Usage: python save_event.py <json_event>'
